89126062c78f0f0a925117ba32b4f2ac4376e2c0
VISHNUVISHAL11 <vishnuvishal.s@techconative.com>
2022-12-30 19:58:10 +0530

    Code fix

--- src/main/java/com/techconative/actions/DozerTOMapperStructPlugin.java
+++ src/main/java/com/techconative/actions/DozerTOMapperStructPlugin.java
@@ -5,12 +5,22 @@ import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.editor.CaretModel;
 import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.fileChooser.FileChooser;
+import com.intellij.openapi.fileChooser.FileChooserDescriptor;
+import com.intellij.openapi.fileChooser.FileChooserFactory;
+import com.intellij.openapi.fileChooser.impl.FileChooserFactoryImpl;
+import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.vfs.VirtualFile;
+
 import com.techconative.actions.service.GenerateMappings;
 import org.jetbrains.annotations.NotNull;
 import javax.swing.*;
 import javax.swing.filechooser.FileSystemView;
+import javax.swing.plaf.FileChooserUI;
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
+import java.util.function.Consumer;
 
 
 public class DozerTOMapperStructPlugin extends AnAction {
@@ -23,6 +33,13 @@ public class DozerTOMapperStructPlugin extends AnAction {
         Editor ediTorRequiredData = e.getRequiredData(CommonDataKeys.EDITOR);
         CaretModel caretModel = ediTorRequiredData.getCaretModel();
         String selectedText = caretModel.getCurrentCaret().getSelectedText();
+        FileChooserDescriptor fileChooserDescriptor=new FileChooserDescriptor(false,true,false,false,false,false);
+        FileChooser.chooseFile(fileChooserDescriptor,e.getProject(),null,  consumer->{
+                Messages.showMessageDialog(e.getProject(),
+                        consumer.toNioPath().normalize().toString(), "Give Path", Messages.getInformationIcon());
+        }
+        );
+
         jfc.setDialogTitle("Choose a directory to save your file: ");
         jfc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
         if (lastPath == null) {
