4329d639e78049adcae15a465599352c24dd8f95
VISHNUVISHAL11 <vishnuvishal.s@techconative.com>
2022-12-31 14:06:01 +0530

    Code fix

--- src/main/java/com/techconative/actions/DozerTOMapperStructPlugin.java
+++ src/main/java/com/techconative/actions/DozerTOMapperStructPlugin.java
@@ -7,56 +7,39 @@ import com.intellij.openapi.editor.CaretModel;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.fileChooser.FileChooser;
 import com.intellij.openapi.fileChooser.FileChooserDescriptor;
-import com.intellij.openapi.fileChooser.FileChooserFactory;
-import com.intellij.openapi.fileChooser.impl.FileChooserFactoryImpl;
 import com.intellij.openapi.ui.Messages;
-import com.intellij.openapi.vfs.VirtualFile;
-
+import com.intellij.openapi.util.Pair;
 import com.techconative.actions.service.GenerateMappings;
 import org.jetbrains.annotations.NotNull;
-import javax.swing.*;
-import javax.swing.filechooser.FileSystemView;
-import javax.swing.plaf.FileChooserUI;
-import java.io.File;
 import java.io.IOException;
-import java.util.List;
-import java.util.function.Consumer;
 
 
 public class DozerTOMapperStructPlugin extends AnAction {
-    static JFileChooser jfc= new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
-    static File lastPath = null;
-
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
 
         Editor ediTorRequiredData = e.getRequiredData(CommonDataKeys.EDITOR);
         CaretModel caretModel = ediTorRequiredData.getCaretModel();
         String selectedText = caretModel.getCurrentCaret().getSelectedText();
-        FileChooserDescriptor fileChooserDescriptor=new FileChooserDescriptor(false,true,false,false,false,false);
-        FileChooser.chooseFile(fileChooserDescriptor,e.getProject(),null,  consumer->{
-                Messages.showMessageDialog(e.getProject(),
-                        consumer.toNioPath().normalize().toString(), "Give Path", Messages.getInformationIcon());
-        }
-        );
+        FileChooserDescriptor fileChooserDescriptor =
+                new FileChooserDescriptor(false, true, false, false, false, false);
+        FileChooser.chooseFile(fileChooserDescriptor, e.getProject(), null, consumer -> {
+                    Messages.showMessageDialog(e.getProject(), "Path selected is :" + consumer.toNioPath().normalize().toString(),
+                            "Give Path", Messages.getInformationIcon());
+                    Pair<String, Boolean> pair = Messages.showInputDialogWithCheckBox("Mapper Class name", "Give Mapper Class Name",
+                            "Generate class", true, true, Messages.getQuestionIcon(), null, null);
+                    String str = Messages.showInputDialog("Give variable name", "Give Variable Name", Messages.getQuestionIcon());
 
-        jfc.setDialogTitle("Choose a directory to save your file: ");
-        jfc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-        if (lastPath == null) {
-            File file = new File(e.getProject().getBasePath().replace("/", "\\"));
-            jfc.setCurrentDirectory(file);
-        }
-        int returnValue = jfc.showSaveDialog(null);
-        if (returnValue == JFileChooser.APPROVE_OPTION && jfc.getSelectedFile().isDirectory()) {
-            lastPath=jfc.getSelectedFile();
-            jfc.setCurrentDirectory(lastPath);
-            System.out.println("You selected the directory: " + lastPath.getAbsolutePath());
+                    System.out.println(pair.first);
+                    System.out.println(pair.second);
+                    System.out.println(str);
             try {
-                GenerateMappings.generateMappings(selectedText,lastPath.getAbsolutePath(),true);
+                GenerateMappings.generateMappings(selectedText, consumer.toNioPath().normalize().toString(), pair.second);
             } catch (IOException ex) {
                 throw new RuntimeException(ex);
             }
-        }
 
+                }
+        );
     }
 }
